8abfc732e1001dea28cbcb153102dcf3518f4954
Olivia Kesler <oakesler@protonmail.ch>
2019-08-22 20:09:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,22 +14,18 @@ class Song
     @@all
   end
   
+   def artist_name=(name)
+    guy = Artist.find_or_create_by_name(name)
+    guy.add_song(self)
+    guy
+  end
+  
   def self.new_by_filename(filename)
-    
     song = self.new(filename.split(" - ")[1])
     artista = artist_name=(filename.split(" - ")[0])
     song.artist = artista
     song
     binding.pry
-  end
-  
-  #artistname=(filename.split(" - ")[0])
-  
-  def artist_name=(name)
-    guy = Artist.find_or_create_by_name(name)
-    guy.add_song(self)
-    guy
-
   end
 end
   
